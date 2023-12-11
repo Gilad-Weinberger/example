3c03c57af80a402e598a38d451eb3b8a837f0d5f
Gilad Weinberger <giladwe21@gmail.com>
2023-12-11 15:28:51 +0200

    add vercel stuff

--- example/settings.py
+++ example/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%@&6&r-s!)ws$4o-c$pyhpgq%0vibo8m%64+x*rqhyar760-%=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
